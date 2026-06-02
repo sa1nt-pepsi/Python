40859eb74e90a420355339ca6270257208e4e1c9
alexe <alexey.ulyanov.2020@gmail.com>
2026-06-02 18:45:10 +0300

    math lessons3

---
+++ lesson_13.py
@@ -0,0 +1,52 @@
+# year = 1900
+# while year <= 2025:
+#     print(year)
+#     year += 1
+# else:
+#     print('Done')
+from sys import stdlib_module_names
+
+# i = 1
+# j = 1
+# while i <= 10:
+#     while j <= 10:
+#         print (i * j, end="\t")
+#         j += 1
+#     print("\n")
+#     j = 1
+#     i += 1
+
+# for i in range(1, 10):
+#     for j in range(2, 10):
+#         print(f'{i} * {j} = {i*j}')
+#     print("-" * 20)
+
+# lesson1
+
+# numbers = [1, 2, 3]
+# print(numbers[0]*2)
+# print(numbers[1]*2)
+# print(numbers[2]*2)
+
+# lesson2
+
+# numbers = [1, 2, 3]
+# print((numbers[0]**2)+(numbers[1]**2)+(numbers[2]**2))
+
+# lesson3
+
+# time1 = 3
+# print(int(time1*0.5))
+# time2 = 6.7
+# print(int(time2*0.5))
+# time3 = 11.8
+# print(int(time3*0.5))
+
+# #lesson4
+
+string = 'Hello World!'
+if ' ' in string:
+    string = string.upper()
+else:
+    string = string.lower()
+print(string)
